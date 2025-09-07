024b5e4ee177ff030e062092bcca9e4fe1a2e472
MaksimIzofatov <giantsgnom@gmail.com>
2025-09-07 22:05:25 +0300

    add logic in aggro script

--- Assets/CodeBase/Enemy/Aggro.cs
+++ Assets/CodeBase/Enemy/Aggro.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace CodeBase.Enemy
@@ -7,23 +8,62 @@ namespace CodeBase.Enemy
     {
         public TriggerObserver TriggerObserver;
         public AgentMoveToPlayer Follow;
+        
+        public float Cooldown;
+        
+        private Coroutine _aggroCoroutine;
+        private bool _hasAggroTarget;
 
         private void Start()
         {
             TriggerObserver.TriggerEnter += TriggerEnter;
             TriggerObserver.TriggerExit += TriggerExit;
             
-            Follow.enabled = false;
-        }
-
-        private void TriggerExit(Collider obj)
-        {
-            
+            SwitchFollowOff();
         }
 
         private void TriggerEnter(Collider obj)
         {
-            
+            if (!_hasAggroTarget)
+            {
+                _hasAggroTarget = true;
+                
+                StopAggroCoroutine();
+                
+                SwitchFollowOn();
+            }
         }
+
+        private void TriggerExit(Collider obj)
+        {
+            if (_hasAggroTarget)
+            {
+                _hasAggroTarget = false;
+            
+                _aggroCoroutine = StartCoroutine(SwitchFollowOffAfterCooldown());
+            }
+        }
+
+        private IEnumerator SwitchFollowOffAfterCooldown()
+        {
+            yield return new WaitForSeconds(Cooldown);
+            
+            SwitchFollowOff();
+        }
+
+        private void StopAggroCoroutine()
+        {
+            if (_aggroCoroutine != null)
+            {
+                StopCoroutine(_aggroCoroutine);
+                _aggroCoroutine = null;
+            }
+        }
+
+        private void SwitchFollowOn() => 
+            Follow.enabled = true;
+
+        private void SwitchFollowOff() => 
+            Follow.enabled = false;
     }
 }
